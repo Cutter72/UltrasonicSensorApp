3988fd80d90bca5471267ed0e2ee56e988e18da8
Pawel Drelich <drelich_pawel@o2.pl>
2021-08-11 18:37:20 +0200

    MeasurementsTimeApproximatorImplTest created. DataFilterImplTest moved.

--- app/src/test/java/com/cutter72/ultrasonicsensor/sensor/activists/DataFilterImplTest.java
+++ app/src/test/java/com/cutter72/ultrasonicsensor/sensor/activists/DataFilterImplTest.java
@@ -1,8 +1,7 @@
-package com.cutter72.ultrasonicsensor;
+package com.cutter72.ultrasonicsensor.sensor.activists;
 
 import androidx.annotation.NonNull;
 
-import com.cutter72.ultrasonicsensor.sensor.activists.DataFilterImpl;
 import com.cutter72.ultrasonicsensor.sensor.solids.Measurement;
 import com.cutter72.ultrasonicsensor.sensor.solids.SensorDataCarrier;
 import com.cutter72.ultrasonicsensor.sensor.solids.SensorDataCarrierImpl;
---
+++ app/src/test/java/com/cutter72/ultrasonicsensor/sensor/activists/MeasurementsTimeApproximatorImplTest.java
@@ -0,0 +1,13 @@
+package com.cutter72.ultrasonicsensor.sensor.activists;
+
+import org.junit.Test;
+
+public class MeasurementsTimeApproximatorImplTest {
+    @Test
+    public void approximateNormal() {
+        //GIVEN
+        //WHEN
+        //THEN
+    }
+
+}
